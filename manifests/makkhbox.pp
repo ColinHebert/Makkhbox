5609aca3c7f9ecd7218cdf8557d7a9e9c65832c9
Colin Hebert <hebert.colin@gmail.com>
2013-05-12 10:15:10 +0200

    Add nginx config for default and transmission

--- manifests/makkhbox.pp
+++ manifests/makkhbox.pp
@@ -5,6 +5,13 @@ include nginx
 include transmission
 include dotfiles
 
+## Set up default site in nginx
+nginx::site {'default':
+    ensure  => present,
+    content => template('makkhbox/nginx/default.conf.erb'),
+}
+
+## Set up transmission in nginx
 nginx::site {'transmission':
     ensure  => present,
     content => template('makkhbox/nginx/transmission.conf.erb'),
