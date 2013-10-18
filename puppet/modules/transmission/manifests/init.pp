443cfa9eff33bf72a10fbd37c349dc4b654a8e47
Colin Hebert <hebert.colin@gmail.com>
2013-10-18 23:50:16 +1100

    remove duplicate content from the transmission class

--- puppet/modules/transmission/manifests/init.pp
+++ puppet/modules/transmission/manifests/init.pp
@@ -1,31 +1,3 @@
 class transmission{
-    $conf_dir = '/var/lib/transmission/.config/transmission-daemon'
 
-    ## Install and start transmission
-    package {'transmission':
-        ensure => installed,
-        name   => 'transmission-cli',
-    }->
-    service {'transmission':
-        enable     => true,
-        ensure     => running,
-        hasrestart => true,
-        name       => 'transmission',
-        restart    => '/usr/bin/killall -HUP transmission-daemon',
-    }
-
-    file {["${conf_dir}/../", "${conf_dir}"]:
-        ensure  => directory,
-        owner   => 'transmission',
-        group   => 'transmission',
-        require => Package['transmission'],
-    }->
-    file {"${conf_dir}/settings.json":
-        ensure  => file,
-        mode    => '0644',
-        content => template("transmission/settings.json.erb"),
-        owner   => 'transmission',
-        group   => 'transmission',
-        notify  => Service['transmission'],
-    }
 }
