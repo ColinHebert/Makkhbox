00d7a2ad1699389c1afaeb1bb3f935a1651d390f
Colin Hebert <hebert.colin@gmail.com>
2013-10-18 23:24:13 +1100

    Install Puppet (and Yaourt if necessary) during bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,3 +5,20 @@ if [ "$EUID" -ne '0' ] ; then
     echo 'Script must be run as root.' >&2
     exit 1
 fi
+
+#################
+# Set up Puppet #
+#################
+if ! type puppet > /dev/null ; then
+
+    if ! type yaourt > /dev/null; then
+        echo 'Installing Yaourt.'
+        echo -e '[archlinuxfr]\nSigLevel = Never\nServer = http://repo.archlinux.fr/$arch' >> /etc/pacman.conf
+        pacman --noconfirm -Sy yaourt base-devel
+        echo 'Yaourt installed.'
+    fi
+
+    echo 'Installing Puppet.'
+    yaourt --noconfirm -S puppet
+    echo 'Puppet installed'
+fi
