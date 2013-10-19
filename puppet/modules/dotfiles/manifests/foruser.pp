4264b177568e38ed35ab46c140b0ca1158da17a4
Colin Hebert <hebert.colin@gmail.com>
2013-10-19 17:28:08 +1100

    Refactor dotfiles::forUser

--- puppet/modules/dotfiles/manifests/foruser.pp
+++ puppet/modules/dotfiles/manifests/foruser.pp
@@ -1,44 +1,47 @@
-define dotfiles::forUser($ensure='present',
-                        $repo = 'https://github.com/ColinHebert/dotfiles.git',
-                        $branch = 'master') {
-    $install_path = '~/.dotfiles'
+define dotfiles::forUser($ensure          = 'present',
+                         $repository_url  = $dotfiles::params::repository_url,
+                         $branch          = $dotfiles::params::branch_name,
+                         $dotfiles_path   = $dotfiles::params::dotfiles_path) {
+    #Validate arguments
 
     case $ensure {
         'present' : {
-
             exec {'dotfilesClone':
-                command  => "git clone --recursive ${repo} ${install_path}",
-                unless   => "test -e ${install_path}",
+                command  => "git clone --recursive ${repo} ${dotfiles_path}",
+                unless   => "test -e ${dotfiles_path}",
                 provider => 'shell',
                 require  => Package['git'],
                 user     => $name,
             }->
             exec {'dotfilesRefresh':
-                command  => "cd ${install_path} && git fetch --all",
+                command  => "cd ${dotfiles_path} && git fetch --all",
                 provider => 'shell',
                 require  => Package['git'],
                 user     => $name,
             }->
             exec {'dotfilesCheckout':
-                command  => "cd ${install_path} && git checkout origin/${branch}",
+                command  => "cd ${dotfiles_path} && git checkout origin/${branch}",
                 provider => 'shell',
                 require  => Package['git'],
                 user     => $name,
             }->
             exec {'dotfilesInstall':
-                command  => "cd ${install_path} && ./install.rb",
+                command  => "cd ${dotfiles_path} && ./install.rb",
                 provider => 'shell',
                 user     => $name,
             }
         }
+
         'absent' : {
             exec {'rmDotfilesLinks':
                 # This will hopefully not break anything
+                # based on dotfiles_path ?
                 command  => 'find ~ -mindepth 1 -maxdepth 1 -type l -ilname "*\.dotfiles*" -exec rm \{\} \;',
                 provider => 'shell',
                 user     => $name,
             }
         }
+
         default : {
             err ("Unknown ensure value: '$ensure'")
         }
