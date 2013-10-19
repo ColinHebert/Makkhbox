713eb06d542dac15a7ac5ed89e6e2d747c8e6a7e
Colin Hebert <hebert.colin@gmail.com>
2013-10-19 22:51:30 +1100

    Only enable transmission

--- puppet/manifests/makkhbox.pp
+++ puppet/manifests/makkhbox.pp
@@ -1,42 +1,7 @@
 Exec { path => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ] }
 exec {'pacman -Sy ':}->Package <| |>
 
-include nginx
 include transmission
-include dotfiles
-
-## Set up default site in nginx
-nginx::site {'default':
-    ensure  => present,
-    content => template('makkhbox/nginx/default.conf.erb'),
-}
-
-## Set up transmission in nginx
-nginx::site {'transmission':
-    ensure  => present,
-    content => template('makkhbox/nginx/transmission.conf.erb'),
-    require => Service['transmission']
-}
-
-## Root settings
-user {'root':
-    shell => '/bin/zsh',
-    require => Package['zsh']
-}->
-dotfiles::forUser {'root':
-    ensure => present,
-    branch  => 'makkhbox',
-}
-
-## Custom user
-user {'makkhdyn':
-    shell => '/bin/zsh',
-    managehome => true,
-    ensure => present,
-    groups => ['transmission', 'wheel'],
-    require => Package['zsh', 'transmission'],
-}->
-dotfiles::forUser {'makkhdyn':
-    ensure  => present,
-    branch  => 'makkhbox',
-}
+#include dotfiles
+#include nginx
+#include makkbox
