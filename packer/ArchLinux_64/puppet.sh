3ff758207ebf6d6dc801959df5fe8b98e593ad9a
Colin Hebert <hebert.colin@gmail.com>
2013-11-03 16:41:10 +1100

    Do not download the documentation and ri with gems

--- packer/ArchLinux_64/puppet.sh
+++ packer/ArchLinux_64/puppet.sh
@@ -4,4 +4,11 @@
 #   aur.sh
 
 yaourt -S --noconfirm puppet
+
+# Don't install RDoc and RI to save time and space
+cat <<EOF >> /etc/gemrc
+install: --no-rdoc --no-ri
+update:  --no-rdoc --no-ri
+EOF
+
 gem install hiera-gpg hiera-file
