87bd3d27f2f39980dc0f258e2d61afff7328894c
Colin Hebert <hebert.colin@gmail.com>
2013-11-30 12:24:38 +1100

    Create a workaround to install nginx without charset issues

--- puppet/modules/nginx/manifests/install.pp
+++ puppet/modules/nginx/manifests/install.pp
@@ -1,5 +1,9 @@
 class nginx::install inherits nginx {
     ## Install nginx
+    # Workaround an issue with yaourt/pacman, puppet and charset
+    exec { "pacman --noconfirm -S nginx":
+      require => Exec['pacman -Sy'],
+    }->
     package { 'nginx':
         ensure => present,
     }
