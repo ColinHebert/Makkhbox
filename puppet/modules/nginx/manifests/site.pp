87d532614183183cb0b66d25c9df310064288ba6
Colin Hebert <hebert.colin@gmail.com>
2013-11-30 13:46:28 +1100

    Ensure that nginx has been included before using it

--- puppet/modules/nginx/manifests/site.pp
+++ puppet/modules/nginx/manifests/site.pp
@@ -4,6 +4,10 @@ define nginx::site(
     $source  = undef
 ) {
 
+    if ! defined(Class['nginx']) {
+        fail('You must include the nginx base class before using any nginx defined resources')
+    }
+
     validate_re($ensure, '^(present|absent)$',
         "${ensure} is not supported for ensure.
         Allowed values are 'present' and 'absent'.")
